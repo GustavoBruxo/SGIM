af5af6f40d2f29105d852f5309eddb0cb3e6311c
GustavoBruxo <gustavo-henrique92@hotmail.com>
2023-01-21 14:43:44 -0300

    APP Imóveis

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Properties(models.Model):
+    TIPOIMOVEL = (
+        ('CS','Casa'),
+        ('AP','Apartamento'),
+        ('LT','Lote'),
+        ('GL','Galpão'),
+        ('','')
+    )
